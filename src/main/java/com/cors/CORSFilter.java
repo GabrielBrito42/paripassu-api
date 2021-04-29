9d7788d251a57a00ba0e118831120248ab10391b
Gabriel Cardoso Brito <32067188+GabrielBrito42@users.noreply.github.com>
2021-04-29 01:02:27 -0300

    Update CORSFilter.java

--- src/main/java/com/cors/CORSFilter.java
+++ src/main/java/com/cors/CORSFilter.java
@@ -20,19 +20,19 @@ public class CORSFilter implements Filter{
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
             throws IOException, ServletException {
 
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
+	HttpServletRequest request = (HttpServletRequest) servletRequest;
 
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+	((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+	((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
 
-        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+	HttpServletResponse resp = (HttpServletResponse) servletResponse;
 
-        if (request.getMethod().equals("OPTIONS")) {
-            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
-            return;
-        }
+	if (request.getMethod().equals("OPTIONS")) {
+	resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+	return;
+	}
 
-        chain.doFilter(request, servletResponse);
+	chain.doFilter(request, servletResponse);
     }
 
     public void init(FilterConfig fConfig) throws ServletException {}
